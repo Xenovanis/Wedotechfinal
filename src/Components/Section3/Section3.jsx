66aeea4d6dc182036e8928ad8a6f13f7efc7334a
Essa05-hub <amaribenaissa1@gmail.com>
2024-09-10 15:41:00 -0700

    section3 done

---
+++ src/Components/Section3/Section3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import img from '../.././assets/transform-the-purple-color-to--53a178-color.png'
+import './Section3.css'
+
+export default function Section3() {
+  return (
+    <div className='section3'>
+      <img src={img} alt="" className='pubimg'/>
+      <p className='pubText'>FIND US<br></br> EVERYWH-<br></br>ERE AND <br></br>ANY WHERE <br></br>YOU GO !!!</p>
+    </div>
+  )
+}
--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Home.css'
 import Section2 from '../../Components/Section2/Section2';
+import Section3 from '../../Components/Section3/Section3';
 
 export default function Home() {
   return (
     <div>
       <Section2></Section2>
+      <Section3></Section3>
     </div>
   )
 }
