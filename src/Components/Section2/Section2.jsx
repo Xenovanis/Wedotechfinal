c63d9ae4f602be3d609ce9d93e995380f3474402
Essa05-hub <amaribenaissa1@gmail.com>
2024-09-10 10:44:09 -0700

    archi

---
+++ src/Components/Section2/Section2.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import img from '../.././assets/pexels-proxyclick-2451646.jpg'
+import '../Section2/Section2.css'
+
+export default function Section2() {
+  return (
+    <div>
+        <img src={img} alt="img" className='imgOfSection1' />
+        <div className="Container"><h1>Let's grow together</h1>
+        <h3>WeDoTech is a full-service media agency that helps businesses <br></br>
+            drive sustainable growth in the new world of marketing. With <br></br>
+            a rich heritage in performance, we apply that mindset to<br></br>
+             everything we do; brand planning, strategy, integrated<br></br>
+              media, social, creative, and data.</h3>
+        </div>
+        <div className="container1">
+            <div className="firstOne">
+            <h1>2K</h1><span>Global Happy Clients</span>
+            <h1>4K</h1><span>Projects Completed</span>
+            </div>
+            <div className="secondtOne">
+            <h1>21 </h1><span>Team members</span>
+            <h1>5</h1><span>Glorious Years</span>
+            </div>
+        </div>
+      
+    </div>
+  )
+}
---
+++ src/Pages/HomePage/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Section2 from '../../Components/Section2/Section2';
+
+export default function Home() {
+  return (
+    <div>
+      <Section2></Section2>
+    </div>
+  )
+}
